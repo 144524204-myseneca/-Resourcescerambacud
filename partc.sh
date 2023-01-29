07df61ef7db4b43c29942afba74ac5c37c2d5016
144524204-myseneca <112990303+144524204-myseneca@users.noreply.github.com>
2023-01-29 14:40:50 -0500

    Add files via upload

---
+++ partc.sh
@@ -0,0 +1,31 @@
+ceram@Bambi MINGW64 ~/Desktop/CSN400 - Prof Atoosa Nasiri/github/CSN400-Capstone/Checkpoint2 (main)
+$ git log -n 5
+commit f088d74606c10e7974032bd080f86ff5e4ffaeab (HEAD -> main, origin/main, origin/feat-emojis, origin/HEAD, feat-emojis)
+Author: cerambacud <cerambacud@myseneca.ca>
+Date:   Sat Jan 28 02:16:14 2023 -0500
+
+    adds emojis to feat-emojis branch
+
+commit 680a208f375d94fc899c7826ee772414910e15f8
+Author: cerambacud <cerambacud@myseneca.ca>
+Date:   Sat Jan 28 02:06:50 2023 -0500
+
+    adds footnotes folder
+
+commit 3ff91cfd6ecabf33338b36a96b18195fd6b02193
+Author: cerambacud <cerambacud@myseneca.ca>
+Date:   Sat Jan 28 01:42:41 2023 -0500
+
+    Adds Part A to README.md file
+
+commit e7383106a5ee24c04494bec4544c7fc154d04a1e
+Author: cerambacud <cerambacud@myseneca.ca>
+Date:   Sat Jan 28 01:12:13 2023 -0500
+
+    adds git_status_committed.txt, git_status_uncommitted.txt, and git_status_untracked.txt
+
+commit 53eb8a758d0d26ba67ba073dae4bf7eb9c3fa7fd
+Author: cerambacud <cerambacud@myseneca.ca>
+Date:   Sat Jan 28 01:09:36 2023 -0500
+
+    adds Checkpoint2/README.md
